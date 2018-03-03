24efad63ffaed326d6a63838ddcdc2bf4804fbb5
keyurpatel23 <37009037+keyurpatel23@users.noreply.github.com>
2018-03-03 15:22:23 +0530

    Update bigchaindb_con.py

--- bigchaindb_con.py
+++ bigchaindb_con.py
@@ -7,34 +7,13 @@ bdb = BigchainDB(
     'https://test.bigchaindb.com',
     headers={'app_id': '64acf62a',
              'app_key': '3b1e2c3b228caf4b9f9a036f1478b4dd'})
-trials = 0
-while (trials < 150):
-	
-	try:
-
-		alice = generate_keypair()
-		prepared_creation_tx = bdb.transactions.prepare(operation='CREATE',signers=alice.public_key,asset={'data': {'message': 'hello 			testing creating'}})
-		fulfilled_creation_tx = bdb.transactions.fulfill(prepared_creation_tx,private_keys=alice.private_key)
-		sent_creation_tx = bdb.transactions.send(fulfilled_creation_tx)
-
-		txid = fulfilled_creation_tx['id']
-		print(txid)
-
-		print(trials)
-		
-		if (bdb.transactions.status(txid).get('status') == 'valid'):
-			print(txid)
-			print(sent_creation_tx == fulfilled_creation_tx)
-			break
-			
-	except e:
-		print(e)
-		print('not valid')
-		
-	trials=trials+1
-
-
-
-
-
-print(bdb.transactions.status(fulfilled_creation_tx['id']))
+i=0
+while(i<50):
+	alice = generate_keypair()
+	tx = bdb.transactions.prepare(operation='CREATE', signers=alice.public_key,asset={'data': {'message': 'keyur'}})
+	signed_tx = bdb.transactions.fulfill(tx,private_keys=alice.private_key)
+	bdb.transactions.send(signed_tx)
+	print(signed_tx['id'])
+	print(bdb.transactions.status(signed_tx['id']))
+	print(i)
+	i=i+1
